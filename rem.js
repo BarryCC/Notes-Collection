205ddb4b0087c3b4d0a45338f468de7a37eee0aa
BarryCC <643987053@qq.com>
2018-10-17 15:46:21 +0800

    get some change

--- rem.js
+++ rem.js
@@ -1,24 +1,57 @@
-(function(c,d){
-    var e=document.documentElement||document.body,
-        a="orientationchange" in window?"orientationchange":"resize",
-        b=function(){
-            var f=e.clientWidth;
-            e.style.fontSize=(f>=750)?"100px":100*(f/750)+"px"
-        };
-    b();
-    c.addEventListener(a,b,false)
-})(window);
-
+/******************* 暂没弄明白 ******************* */
 
 (function(doc, win) {
-    var docEl = doc.documentElement,
+    var docEl = doc.documentElement || document.body,
         resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
         recalc = function() {
             var clientWidth = docEl.clientWidth;
             if (!clientWidth) return;
+            //docEl.style.fontSize = (f>=750) ? "100px":100*(f/750)+"px";
             docEl.style.fontSize = 32 * (clientWidth / 320) + 'px';
         };
-    if (!doc.addEventListener) return;
     win.addEventListener(resizeEvt, recalc, false);
     doc.addEventListener('DOMContentLoaded', recalc, false);
 })(document, window);
+
+
+(function flexible (win, doc) {
+    var docEl = doc.documentElement;
+    var dpr = win.devicePixelRatio || 1;
+    
+    // adjust body font size
+    function setBodyFontSize() {
+        if(doc.body) {
+            doc.body.style.fontSize = (12 * dpr) + 'px';
+        }else{
+            doc.addEventListener('DOMContentLoaded', setBodyFontSize);
+        }
+    }
+    setBodyFontSize();
+
+    // set 1rem = viewWidth / 10;
+    function setRemUnit() {
+        var rem = docEl.clientWidth / 10;
+        docEl.style.fontSize = rem + 'px';
+    }
+    setRemUnit();
+    // reset rem unit on page resize
+    win.addEventListener('resize', setRemUnit);
+    window.addEventListener('pageshow', function(e) {
+        if(e.persisted) {
+            setRemUnit();
+        }
+    });
+
+    // detect 0.5px suports  ???
+    if(dpr >= 2) {
+        var fakeBody = doc.createElement('body');
+        var testElement = doc.createElement('div');
+        testElement.style.border = '0.5px solid transparent';
+        fakeBody.appendChild(testElement);
+        docEl.appendChild(fakeBody);
+        if(testElement.offsetHeight === 1) {
+            docEl.classList.add('');
+        }
+        docEl.removeChild(fakeBody);
+    }
+})(window, document);
